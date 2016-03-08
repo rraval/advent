b58806fd7aa9bfc3dd16489c799daf77f3b27709
Ronuk Raval <ronuk.raval@gmail.com>
2016-03-07 23:39:46 -0500

    [day7] add some tests

--- day7/test/Spec.hs
+++ day7/test/Spec.hs
@@ -10,4 +10,17 @@ main = runTestTT $ TestList
             [Store { dst = WireName "a", expr = Atom $ Literal $ LiteralValue 16 }]
             (WireName "a")
         )
+
+    , TestCase $ assertEqual "Simple Cycle"
+        (Left $ CycleDetected [WireName "x", WireName "y", WireName "x"])
+        (eval
+            [ Store { dst = WireName "x", expr = Atom $ Wire $ WireName "y" }
+            , Store { dst = WireName "y", expr = Atom $ Wire $ WireName "x" }
+            ]
+            (WireName "x")
+        )
+
+    , TestCase $ assertEqual "Invalid Lookup"
+        (Left $ NoSuchWire $ WireName "x")
+        (eval [] (WireName "x"))
     ]
