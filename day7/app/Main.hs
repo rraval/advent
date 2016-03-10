f2464c91e82e85743463f75d46ff0f07b689f838
Ronuk Raval <ronuk.raval@gmail.com>
2016-03-09 23:27:53 -0500

    [day7] add a main

--- day7/app/Main.hs
+++ day7/app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
+import System.Environment (getArgs)
+import Text.Trifecta (parseFromFile)
+
 import Data
+import Eval
+import Parser
 
 main :: IO ()
-main = putStrLn "hello, world"
+main = getArgs >>= go
+  where
+    go (file:wire:_) = runProgram file wire >>= print
+    go _ = putStrLn "Usage: day7 FILE WIRE"
+
+runProgram :: String -> String -> IO (Maybe LookupResult)
+runProgram file wire = do
+    insts <- parseFromFile instructionListParser file
+    return $ (flip eval) (WireName wire) <$> insts
