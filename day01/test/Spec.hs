05465d89d74ce2d145eeddca80b8855edef8ad9f
Ronuk Raval <ronuk.raval@gmail.com>
2016-03-11 23:55:47 -0500

    [day1] use ~?= operator

--- day01/test/Spec.hs
+++ day01/test/Spec.hs
@@ -4,30 +4,30 @@ import Lib
 
 main = runTestTT $ TestList
     [ TestLabel "first" $ TestList
-        [ TestCase $ count "" @?= 0
+        [ count "" ~?= 0
 
-        , TestCase $ count "(())" @?= 0
-        , TestCase $ count "()()" @?= 0
+        , count "(())" ~?= 0
+        , count "()()" ~?= 0
 
-        , TestCase $ count "(((" @?= 3
-        , TestCase $ count "(()(()(" @?= 3
+        , count "(((" ~?= 3
+        , count "(()(()(" ~?= 3
 
-        , TestCase $ count "))(((((" @?= 3
+        , count "))(((((" ~?= 3
 
-        , TestCase $ count "())" @?= -1
-        , TestCase $ count "))(" @?= -1
+        , count "())" ~?= -1
+        , count "))(" ~?= -1
 
-        , TestCase $ count ")))" @?= -3
-        , TestCase $ count ")())())" @?= -3
+        , count ")))" ~?= -3
+        , count ")())())" ~?= -3
         ]
 
     , TestLabel "second" $ TestList
-        [ TestCase $ findPos 0 "" @=? Just 0
-        , TestCase $ findPos 1 "" @=? Nothing
+        [ findPos 0 "" ~?= Just 0
+        , findPos 1 "" ~?= Nothing
 
-        , TestCase $ findPos (-1) ")" @=? Just 1
-        , TestCase $ findPos (-1) "()())" @=? Just 5
+        , findPos (-1) ")" ~?= Just 1
+        , findPos (-1) "()())" ~?= Just 5
 
-        , TestCase $ findPos (-1) "()()(()))(" @=? Just 9
+        , findPos (-1) "()()(()))(" ~?= Just 9
         ]
     ]
